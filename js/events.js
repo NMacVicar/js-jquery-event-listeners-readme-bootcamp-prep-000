9b07ecdb9b8852a704a0c41086486f8da5e22df6
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-27 16:45:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,30 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $().on('frame', function(){
+    alert()
+  })
+}
+
+function pressIt(){
+  $().on('press', function(){
+    alert()
+  })
+}
+
+function submitIt(){
+  $().on('submit', function(){
+    alert()
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
